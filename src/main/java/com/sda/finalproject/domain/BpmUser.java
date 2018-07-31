c6bd4f940508430231f5f135f89846c2a90ce8e6
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-07-31 20:11:06 +0200

    piotr commit add task

---
+++ src/main/java/com/sda/finalproject/domain/BpmUser.java
@@ -0,0 +1,133 @@
+package com.sda.finalproject.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
+@Entity
+public class BpmUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+
+
+//    public BpmUser(String email, String password, String password_confirm) {
+//        this.email = email;
+//        this.password = password;
+//        this.password_confirm = password_confirm;
+//    }
+//
+//    public BpmUser(String email, String name, String surname, BpmUserSubjectExpert bpmUserSubjectExpert) {
+//        this.email = email;
+//        this.name = name;
+//        this.surname = surname;
+//        this.bpmUserSubjectExpert = bpmUserSubjectExpert;
+//    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority simpleGrantedAuthority =
+                new SimpleGrantedAuthority("ROLE_USER");
+        return Collections.singleton(simpleGrantedAuthority);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+
+
+    public BpmUser() {
+    }
+
+    public BpmUser(String email, String name, String surname) {
+        this.email = email;
+        this.name = name;
+        this.surname = surname;
+
+    }
+
+    public static BpmUser createList(BpmUser bpmUser) {
+        return new BpmUser(bpmUser.getEmail(), bpmUser.getName(), bpmUser.getSurname());
+    }
+
+
+}
---
+++ src/main/java/com/sda/finalproject/model/LoginGui.java
@@ -0,0 +1,84 @@
+package com.sda.finalproject.model;
+
+import com.sda.finalproject.domain.BpmUser;
+import com.sda.finalproject.manger.LoginManager;
+import com.sda.finalproject.security.ISecurity;
+import com.vaadin.server.Page;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+@SpringUI(path = "/login")
+public class LoginGui extends UI {
+
+    @Autowired
+    private LoginManager loginManager;
+
+    @Autowired
+    ISecurity iSecurity;
+
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+
+        VerticalLayout registerLayout = new VerticalLayout();
+
+        registerLayout.setSpacing(false);
+        registerLayout.setMargin(false);
+        registerLayout.setCaption("Login");
+
+        TextField emailField = new TextField("E-mail");
+        PasswordField passwordField = new PasswordField("Password");
+
+        Button loginButton = new Button("Login");
+
+        loginButton.addClickListener(event ->
+        {
+
+            if (!emailField.getValue().isEmpty() && !passwordField.getValue().isEmpty()) {
+
+                Optional<BpmUser> userByEmail = loginManager.getPersonByEmail(emailField.getValue());
+                if (userByEmail.isPresent()) {
+                    if (userByEmail.get().getPassword().equals(passwordField.getValue())) {
+
+                        iSecurity.autoLogin(userByEmail.get().getEmail(), userByEmail.get().getPassword());
+                        Notification.show("Hello " + userByEmail.get().getName(), Notification.Type.TRAY_NOTIFICATION);
+//                        Page.getCurrent().open("/user-data?userId=" + userByEmail.get().getId(), null);
+                        Page.getCurrent().open("/task-list", null);
+
+                    } else {
+                        Notification.show("Incorrect password!", Notification.Type.ERROR_MESSAGE);
+                    }
+
+
+                } else {
+                    Notification.show("Cannot find the user", Notification.Type.ERROR_MESSAGE);
+                    Page.getCurrent().open("/register", null);
+                }
+
+
+            }
+
+
+        });
+
+        registerLayout.addComponent(emailField);
+        registerLayout.setComponentAlignment(emailField, Alignment.MIDDLE_CENTER);
+        registerLayout.addComponent(passwordField);
+        registerLayout.setComponentAlignment(passwordField, Alignment.MIDDLE_CENTER);
+        registerLayout.addComponent(loginButton);
+        registerLayout.setComponentAlignment(loginButton, Alignment.MIDDLE_CENTER);
+
+        setContent(registerLayout);
+
+
+
+
+    }
+
+
+}
